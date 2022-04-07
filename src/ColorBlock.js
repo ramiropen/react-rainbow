cf6e1218a7cbf154b7e6d2b1254394b376755f6f
unknown <ramiropen.work@gmail.com>
2022-04-07 15:32:31 -0700

    ahrd to accomplish goal without all of the curriculum

--- src/ColorBlock.js
+++ src/ColorBlock.js
@@ -1,31 +1,24 @@
 import React, { useState } from 'react'
+// Make sure to import the component we just built:
 import ColorBlock from './ColorBlock'
 
 function App(){
-    let [colors, setColors] = useState([
+    let colors = [
         'violet', 'blue',
         'lightblue', 'green',
         'greenyellow', 'yellow',
         'orange', 'red'
-    ])
-
- 
-}
-
-    const addColor = (newColor) => {
-        setColors([...colors, newColor])
-    }
+    ]
+    
+    let colorMap = colors.map((color, i) => {
+        return (
+            <ColorBlock color={color} />
+        )
+    })
 
     return (
         <div className="App">
             {colorMap}
-            <ColorForm addColor={addColor} />
         </div>
     )
-
-
-
-
-export default App;
-
-
+}
